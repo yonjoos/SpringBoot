de3773e28350896725df40f858d7de48d77f9971
yonjoos <joyce977@naver.com>
2023-08-03 04:40:07 +0900

    JPA와 DB설정, 동작확인

---
+++ core2/src/main/java/jpabook/jpashop/Member.java
@@ -0,0 +1,21 @@
+package jpabook.jpashop;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+public class Member {
+    @Id @GeneratedValue
+    private Long id;
+    private String username;
+
+    public void SetUsername(String memberA) {
+        username = memberA;
+    }
+}
---
+++ core2/src/main/java/jpabook/jpashop/MemberRepository.java
@@ -0,0 +1,24 @@
+package jpabook.jpashop;
+
+
+import org.springframework.stereotype.Repository;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberRepository {
+    @PersistenceContext
+    private EntityManager em;
+
+    //저장하는 코드
+    public Long save(Member member){
+        em.persist(member); //member 집어넣음
+        return member.getId();
+    }
+
+    public Member find(Long id){
+        return em.find(Member.class, id);
+    }
+
+
+}
