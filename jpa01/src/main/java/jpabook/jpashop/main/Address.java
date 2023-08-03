2c0d8829581aca12e572737d33cfe77100edc370
yonjoos <joyce977@naver.com>
2023-08-03 22:56:31 +0900

    JPA, Section 2 fin

--- jpa01/src/main/java/jpabook/jpashop/main/Address.java
+++ jpa01/src/main/java/jpabook/jpashop/main/Address.java
@@ -6,10 +6,21 @@ import lombok.Setter;
 
 import javax.persistence.Embeddable;
 
-@Embeddable
-@Getter @Setter
+@Embeddable //값타입, 수정이 불가능함, 생성할 때만 입력 가능함, thus setter 없음
+@Getter
 public class Address {
+
+    //기본 생성자 없으면 안 된다고 하기는 하는데
+    protected Address() {}
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
+
     private String city;
     private String street;
     private String zipcode;
+
 }
