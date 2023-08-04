fcc0fdf7a39ca3dec54505d94722a98eb84c1df5
yonjoos <joyce977@naver.com>
2023-08-05 02:10:24 +0900

    JPA, Section4, 회원 서비스 개발, only using @RequiredArgsConstructor

--- jpa01/src/main/java/jpabook/jpashop/service/MemberService.java
+++ jpa01/src/main/java/jpabook/jpashop/service/MemberService.java
@@ -17,34 +17,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class MemberService {
 
-/*
-
-##### SETTER INJECTION 보다는 아래의 생성자 INJECTION이 better
-    @Autowired
-    private MemberRepository memberRepository;
-
-    @Autowired //setter injection
-    public void setMemberRepository(MemberRepository memberRepository){
-        this.memberRepository = memberRepository;
-    }
-
-*/
-
-
-/*
-########## LOMBOK(@AllArgsConstructor, @RequiredArgsConstructor), 쓰면 생성자 injection 을 더 간결하게 만들 수 있음
-
----@AllArgsConstructor
----@RequiredArgsConstructor : final 붙은 것만 만들어줌
-
-
-private final MemberRepository memberRepository;
-    @Autowired //생성자 injection
-    public MemberService(MemberRepository memberRepository){
-        this.memberRepository = memberRepository;
-    }
-    
-*/
+    //@RequiredArgsConstructor 쓰면, final 붙은 것만 생성자 만들어줌
     private final MemberRepository memberRepository;
 
 
