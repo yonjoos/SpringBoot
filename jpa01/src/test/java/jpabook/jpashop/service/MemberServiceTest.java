d2efc39545dd46e25bff621b4951835552e2614e
yonjoos <joyce977@naver.com>
2023-08-05 02:31:21 +0900

    JPA, Section 4, 회원 기능 테스트, RollBack(false) -> insert 실행 OK
    @RollBack 이거 안 쓰면 insert 안 됨, 테스트에서는

--- jpa01/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ jpa01/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,6 +22,7 @@ public class MemberServiceTest {
     @Autowired MemberRepository memberRepository;
 
     @Test //Are there any same IDs? in A TRANSACTION
+    @Rollback(value = false)
     public void register() throws Exception{
         //given
         Member member = new Member();
