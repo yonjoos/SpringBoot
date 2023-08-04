e490b0708928712e24e276cca86f604d9289bc32
yonjoos <joyce977@naver.com>
2023-08-05 02:30:03 +0900

    JPA, Sectio 4, 회원 기능 테스트

---
+++ jpa01/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,45 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.main.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.*;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest //메모리랑 이런거 한꺼번에 테스트 할 꺼라서 위에 2개 annotations 꼭 쓰세용
+@Transactional //for "RollBack"
+public class MemberServiceTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
+    @Test //Are there any same IDs? in A TRANSACTION
+    public void register() throws Exception{
+        //given
+        Member member = new Member();
+        member.setUsername("kim");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        assertEquals(member, memberRepository.findOne(saveId));
+    }
+
+    @Test
+    public void existing_exception() throws Exception{
+        //given
+
+        //when
+
+        //then
+    }
+
+}
