7197b7a8eabbd411c428d02015c8ce2eddcd6b79
yonjoos <joyce977@naver.com>
2023-08-05 22:06:55 +0900

    JPA, Section 6, 주문 리포지토리 개발, (orderSearch class 덜 만들어져서 안 돌아감)

---
+++ jpa01/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -0,0 +1,29 @@
+package jpabook.jpashop.repository;
+
+
+import jpabook.jpashop.main.Order;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+
+    private final EntityManager em;
+
+    public void save(Order order){
+        em.persist(order);
+    }
+
+    public Order findOne(Long id){
+        return em.find(Order.class, id);
+    }
+
+
+    //나중에 더 설명하겠음
+    public List<Order> findAll(OrderSearch orderSearch) {}
+
+}
