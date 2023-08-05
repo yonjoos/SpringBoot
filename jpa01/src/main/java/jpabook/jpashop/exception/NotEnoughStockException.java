7c7159f14032ad2f98be930acb05279f312f885b
yonjoos <joyce977@naver.com>
2023-08-05 12:29:17 +0900

    JPA, Section 5, 상품 엔티티 개발(비즈니스 로직 추가)

---
+++ jpa01/src/main/java/jpabook/jpashop/exception/NotEnoughStockException.java
@@ -0,0 +1,24 @@
+package jpabook.jpashop.exception;
+
+public class NotEnoughStockException extends RuntimeException{
+    public NotEnoughStockException() {
+        super();
+    }
+
+    public NotEnoughStockException(String message) {
+        super(message);
+    }
+
+    public NotEnoughStockException(String message, Throwable cause) { //Throwable cause : message가 발생한 근원적 Exception을 넣어
+        super(message, cause);
+    }
+
+    public NotEnoughStockException(Throwable cause) {
+        super(cause);
+    }
+
+    protected NotEnoughStockException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+}
--- jpa01/src/main/java/jpabook/jpashop/main/item/Item.java
+++ jpa01/src/main/java/jpabook/jpashop/main/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.main.item;
 
 
+import jpabook.jpashop.exception.NotEnoughStockException;
 import jpabook.jpashop.main.Category;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,7 +12,7 @@ import java.util.List;
 
 @Entity
 @Getter
-@Setter
+//Setter(바깥에서)를 넣는 게 아니라, 비즈니르 로직(Entity 안에서, 객체지향적)을 가지고 변경 해야함
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE) //싱글테이블 전략
 @DiscriminatorColumn(name = "dtype")
 public abstract class Item {
@@ -26,4 +27,23 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items") //Category java 안에 있는 변수명
     private List<Category> categories = new ArrayList<>();
+
+
+    //======== 비즈니스 로직 ===========
+    //Entitiy 안에 조절하는 로직이 있어야돼용
+
+
+    // 제고 증가
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity) {
+        int realStock = stockQuantity - quantity;
+        if (realStock < 0)
+            throw new NotEnoughStockException("need more stock"); //Exception 만들어야됨
+        this.stockQuantity = realStock;
+    }
+
+
 }
