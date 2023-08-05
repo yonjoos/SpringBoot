c35d40f8950564b505bf1223d4fd606cc4b79602
yonjoos <joyce977@naver.com>
2023-08-05 21:58:13 +0900

    JPA, Section 6, 주문, 주문상품 엔티티 개발, 자바 문법으로 표현
    .stream() 관련

--- jpa01/src/main/java/jpabook/jpashop/main/Order.java
+++ jpa01/src/main/java/jpabook/jpashop/main/Order.java
@@ -89,11 +89,7 @@ public class Order {
     //====조회 로직=====
     //전체 주문 가격 조회
     public int getTotalPrice(){
-        int totalPrice = 0;
-        for(OrderItem orderItem : orderItems){
-            totalPrice += orderItem.getTotalPrice();
-
-        }
+        int totalPrice = orderItems.stream().mapToInt(OrderItem::getTotalPrice).sum();
         return totalPrice;
     }
 
