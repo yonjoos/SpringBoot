069bb6598a745c6b6d6c45994be453e1409a518f
yonjoos <joyce977@naver.com>
2023-08-07 04:16:46 +0900

    JPA, Section 7, 상품 목록

--- jpa01/src/main/java/jpabook/jpashop/controller/ItemController.java
+++ jpa01/src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -1,6 +1,6 @@
 package jpabook.jpashop.controller;
 
-
+import jpabook.jpashop.main.item.Item;
 import jpabook.jpashop.main.item.Book;
 import jpabook.jpashop.service.ItemService;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -34,5 +36,12 @@ public class ItemController {
 
         itemService.saveItem(book);
         return "redirect:/";
+    }
+
+    @GetMapping("/items")
+    public String list(Model model){
+        List<Item> items = itemService.findItems();
+        model.addAttribute("items", items);
+        return "items/itemList";
     }
 }
