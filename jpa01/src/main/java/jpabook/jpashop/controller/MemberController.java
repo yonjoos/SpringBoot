91dd65ca32db8de980ad558f833ad2030f4fb8bc
yonjoos <joyce977@naver.com>
2023-08-07 03:37:50 +0900

    JPA, Section 7, 회원 등록

---
+++ jpa01/src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -0,0 +1,44 @@
+package jpabook.jpashop.controller;
+
+
+import jpabook.jpashop.main.Address;
+import jpabook.jpashop.main.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+    private final MemberService memberService;
+
+    @GetMapping("/members/new")
+    public String createForm(Model model){
+        /*
+        What is MODEL??........
+         */
+        model.addAttribute("memberForm", new MemberForm()); //memberForm이라는 빈 껍데기를...
+        return "members/createMemberForm";
+
+    }
+
+    @PostMapping("/members/new") //데이터를 실제로 등록
+    public String create(@Valid MemberForm form)
+    {
+        //@Valid : MemberForm에 있는 @NotEmpty...이거 불러와줌
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+
+        Member member = new Member();
+        member.setUsername(form.getName());
+        member.setAdress(address);
+
+        memberService.join(member);
+
+        return "redirect:/"; //첫 번째 페이지로 넘어가용
+    }
+}
