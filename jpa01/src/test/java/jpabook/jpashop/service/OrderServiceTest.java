b3460b42d682c9ae796dccab251989c1b85d7aeb
yonjoos <joyce977@naver.com>
2023-08-06 15:49:53 +0900

    JPA, Section 6, 주문 기능 테스트, 상품주문_재고수량초과(), Error

--- jpa01/src/test/java/jpabook/jpashop/service/OrderServiceTest.java
+++ jpa01/src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.service;
 
 
+import jpabook.jpashop.exception.NotEnoughStockException;
 import jpabook.jpashop.main.Address;
 import jpabook.jpashop.main.Member;
 import jpabook.jpashop.main.Order;
@@ -17,6 +18,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+
+import static org.junit.jupiter.api.Assertions.fail;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -37,17 +40,12 @@ public class OrderServiceTest {
     @Test
     public void 상품주문() throws Exception{
         //given
-        Member member = new Member();
-        member.setUsername("회원1");
-        member.setAdress(new Address("서울", "강가", "123-123"));
-        em.persist(member);
+        Member member = createMember();
 
 
-        Book book = new Book();
-        book.setName("시골 JPA");
-        book.setPrice(10000);
-        book.setStockQuantity(10);
-        em.persist(book);
+
+        Book book = createBook("시골", 10000, 10);
+
 
 
         int orderCount = 2;
@@ -65,6 +63,25 @@ public class OrderServiceTest {
 
     }
 
+
+
+    @Test(expected = NotEnoughStockException.class)
+    public void 상품주문_재고수량초과() throws Exception{
+        //given
+        Member member = createMember();
+        Item item = createBook("시골", 10000, 10);
+
+        int orderCount = 11; //exception 생겨야함
+
+
+        //when
+        orderService.order(member.getId(), item.getId(), orderCount);
+
+        //then
+        fail("재고수량 예외 생겨야함");
+
+    }
+
     @Test
     public void 주문취소() throws  Exception{
         //given
@@ -76,11 +93,27 @@ public class OrderServiceTest {
 
     @Test
     public void 재고수량_초과() throws Exception{
-        //given
 
-        //when
+    }
 
-        //then
+
+
+    //command + opt + M
+    private Book createBook(String name, int price, int stockQuantity) { //command + opt + P
+        Book book = new Book();
+        book.setName(name);
+        book.setPrice(price);
+        book.setStockQuantity(stockQuantity);
+        em.persist(book);
+        return book;
+    }
+
+    private Member createMember() {
+        Member member = new Member();
+        member.setUsername("회원1");
+        member.setAdress(new Address("서울", "강가", "123-123"));
+        //em.persist(member);
+        return member;
     }
 
 
