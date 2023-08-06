c006290dbd84bf048f48f48f9eff63cb0e683c26
yonjoos <joyce977@naver.com>
2023-08-07 03:41:43 +0900

    JPA, Section 7, 회원 등록(NotEmpty 조건 만족 못할 경우)

--- jpa01/src/main/java/jpabook/jpashop/controller/MemberController.java
+++ jpa01/src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -7,6 +7,7 @@ import jpabook.jpashop.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -28,8 +29,11 @@ public class MemberController {
     }
 
     @PostMapping("/members/new") //데이터를 실제로 등록
-    public String create(@Valid MemberForm form)
+    public String create(@Valid MemberForm form, BindingResult result)  //오류가 있으면 오류가 담겨서 코드가 실행됨
     {
+        //타임리프-스프링 라이브러리 참고하세요
+        if(result.hasErrors()){
+            return "members/createMemberForm"; //어떤 에러가 있는지 이 화면에서 확인 가능, MemberForm에 @NotEmpty에 적어놨던 에러
         //@Valid : MemberForm에 있는 @NotEmpty...이거 불러와줌
         Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
 
@@ -39,6 +43,6 @@ public class MemberController {
 
         memberService.join(member);
 
-        return "redirect:/"; //첫 번째 페이지로 넘어가용
+        return "redirect:/"; //첫 번째 페이지로 넘어가
     }
 }
