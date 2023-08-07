ff4c572dbfba72ef850710355a4dcd9cad78d03c
yonjoos <joyce977@naver.com>
2023-08-08 06:50:21 +0900

    ORM, Section 2, 애플리케이션 개발

---
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,19 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args){
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+        //실제 동작 코드
+        em.close();
+
+        emf.close();
+
+    }
+}
