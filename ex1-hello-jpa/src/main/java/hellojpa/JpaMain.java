7ec5a127bb18dba86eae909ebdf2846a27577d32
yonjoos <joyce977@naver.com>
2023-08-08 07:09:38 +0900

    ORM, Section 2, 애플리케이션 개발

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -2,6 +2,7 @@ package hellojpa;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class JpaMain {
@@ -10,6 +11,10 @@ public class JpaMain {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
 
         EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin(); //database transaction 시작
+
         //실제 동작 코드
         Member member = new Member();
 
@@ -17,6 +22,7 @@ public class JpaMain {
         member.setName("HelloA");
 
         em.persist(member);
+        tx.commit(); //transaction CLOSE
 
         em.close();
 
