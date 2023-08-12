72f66792a4ebe3452e821c03d3f17c3907695d4b
yonjoos <joyce977@naver.com>
2023-08-12 21:51:00 +0900

    ORM, Section 6, flush 전후의 변화 살펴보기

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args){
@@ -20,54 +21,109 @@ public class JpaMain {
             team.setName("TeamA");
             em.persist(team);
 
+
+            Team team2 = new Team();
+            team2.setName("TeamB");
+            em.persist(team2);
+
+
             Member member = new Member();
             member.setUsername("member1");
-
             member.setTeam(team);
-            team.getMemberList().add(member);
-
+            //team.getMemberList().add(member);
             em.persist(member);
 
-
-            //########## Member 객체는 그대로, 이름만 바꿔 넣으면 : 영속성에서 Id 그대로
-            member.setUsername("member2");
-            member.setTeam(team);
-            em.persist(member);
-
-            team.getMemberList().add(member);
 
             //########## Member 객체 새로 생성 : 영속성에서 Id 바뀜
             Member member2 = new Member();
-            member.setUsername("member3");
-            member.setTeam(team);
-            team.getMemberList().add(member2);
-
+            member2.setUsername("member3");
+            member2.setTeam(team);
+            //team.getMemberList().add(member2);
             em.persist(member2);
 
 
+
+            Team findTeam1 = em.find(Team.class, team.getId());
+            Team findTeam2 = em.find(Team.class, team2.getId());
+            List<Member> members = findTeam1.getMemberList();
+            List<Member> members2 = findTeam2.getMemberList();
+
+            System.out.println("######## BEFORE FLUSH ##########");
+            System.out.println("######## BEFORE FLUSH ##########");
+            System.out.println("######## BEFORE FLUSH ##########");
+
+            System.out.println("member 1의 이름 " + member.getUsername());
+            System.out.println("member 2의 이름 " + member2.getUsername());
+            System.out.println("member 1의 팀 " + member.getTeam().getName());
+            System.out.println("member 2의 팀 " + member2.getTeam().getName());
+            System.out.println("Team 1의 목록 " + team.getMemberList());
+            System.out.println("Team 2의 목록 " + team2.getMemberList());
+            System.out.println("Members 1의 목록 " + members);
+            System.out.println("Members 2의 목록 " + members2);
+
+
+            member2.setTeam(team2);
+
             em.flush();
+            em.clear();
 
-            Member findMember = em.find(Member.class, member.getId());
+            findTeam1 = em.find(Team.class, team.getId());
+            findTeam2 = em.find(Team.class, team2.getId());
+            members = findTeam1.getMemberList(); //다시 찾음
+            members2 = findTeam2.getMemberList(); //다시 찾음
+            List<Member> members3 = findTeam1.getMemberList();
+            List<Member> members4 = findTeam2.getMemberList();
 
-            //Team findTeam = findMember.getTeam();
-            Team findTeam = em.find(Team.class, team.getId());
-            int memberSize = findTeam.getMemberList().size();
-            Member member_1 = findTeam.getMemberList().get(0);
-            Member member_2 = findTeam.getMemberList().get(1);
-            Member member_3 = findTeam.getMemberList().get(2);
+            System.out.println("######## AFTER FLUSH ##########");
+            System.out.println("######## AFTER FLUSH ##########");
+            System.out.println("######## AFTER FLUSH ##########");
 
-            System.out.println("============" + memberSize);
-            System.out.println("=============" + member_1);
-            System.out.println("=============" + member_2);
-            System.out.println("=============" + member_3);
+            System.out.println("member 1의 이름 " + member.getUsername());
+            System.out.println("member 2의 이름 " + member2.getUsername());
+            System.out.println("member 1의 팀 " + member.getTeam().getName());
+            System.out.println("member 2의 팀 " + member2.getTeam().getName());
+            System.out.println("(객체)Team 1의 목록 " + team.getMemberList()); //empty
+            System.out.println("(객체)Team 2의 목록 " + team2.getMemberList()); //empty
+            System.out.println("Members 1의 목록 " + members); //empty
+            System.out.println("Members 2의 목록 " + members2);  //empty
+            System.out.println("Members 1(2)의 목록 " + members3); //다시 찾음
+            System.out.println("Members 2(2)의 목록 " + members4); //다시 찾음
+
+
+
+            //결론, flush를 해도 DB값을 객체에 담으려면 em.find() 해줘야한다.
+
 
 
             tx.commit(); //transaction CLOSE
 
+
+
+            Team findTeam3 = em.find(Team.class, team.getId());
+            Team findTeam4 = em.find(Team.class, team2.getId());
+            List<Member> members5 = findTeam3.getMemberList();
+            List<Member> members6 = findTeam4.getMemberList();
+
+
+            System.out.println("######## AFTER TX ##########");
+            System.out.println("######## AFTER TX ##########");
+            System.out.println("######## AFTER TX ##########");
+
+            System.out.println("member 1의 이름 " + member.getUsername());
+            System.out.println("member 2의 이름 " + member2.getUsername());
+            System.out.println("member 1의 팀 " + member.getTeam().getName());
+            System.out.println("member 2의 팀 " + member2.getTeam().getName());
+            System.out.println("(객체)Team 1의 목록 " + team.getMemberList());
+            System.out.println("(객체)Team 2의 목록 " + team2.getMemberList());
+            System.out.println("Members 1의 목록 " + members);
+            System.out.println("Members 2의 목록 " + members2);
+            System.out.println("Members 1(2)의 목록 " + members5); //다시 안 찾음
+            System.out.println("Members 2(2)의 목록 " + members6); //다시 안 찾음
         }catch (Exception e){ // IF ERROR OCCURS
             tx.rollback();
         }finally{ // WHEN THE TASK IS DONE
             em.close(); // DATABASE CONNECTION OFF
+
         }
 
         emf.close();
