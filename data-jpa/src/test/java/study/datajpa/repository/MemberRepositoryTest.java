3be9050fd6ffe3aff6e1b08dc0903a50f224b0f2
yonjoos <joyce977@naver.com>
2023-08-21 22:15:53 +0900

    스프링 데이터 JPA, Section 4, 파라미터 바인딩

--- data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -10,6 +10,7 @@ import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.entity.Team;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -130,6 +131,26 @@ class MemberRepositoryTest {
 
         List<MemberDto> memberDto = memberRepository.findMemberDto();
         for(MemberDto s : memberDto){
+            System.out.println("s = " + s);
+        }
+
+
+    }
+
+
+    @Test
+    public void findByNames(){
+
+        Team team = new Team("teamA");
+        teamRepository.save(team);
+
+
+        Member m1 = new Member("AAA", 10);
+        m1.setTeam(team);
+        memberRepository.save(m1);
+
+        List<Member> memberDto = memberRepository.findByNames(Arrays.asList("AAA", "BBB"));
+        for(Member s : memberDto){
             System.out.println("s = " + s);
         }
 
